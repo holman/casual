dd1853188be09720a4eaf34e3998d181971ade61
Zach Holman <github.com@zachholman.com>
2010-08-06 18:13:46 -0700

    the I hate Jasig version

--- lib/casual.rb
+++ lib/casual.rb
@@ -3,12 +3,14 @@ require 'hpricot'
 
 module Casual
   class Client
-    attr_accessor :server_path, :callback_url, :port
+    attr_accessor :hostname, :path, :https, :port, :callback_url, :jasig
 
     def initialize(config)
-      @server_path = config[:server_path]
+      @hostname     = config[:hostname]
+      @path         = config[:path]
+      @https        = config[:https]
+      @port         = config[:port] || 443
       @callback_url = config[:callback_url]
-      @port = config[:port] || 443
     end
 
     def authorization_url
@@ -18,32 +20,59 @@ module Casual
     def authenticate(username,password)
       ticket = acquire_ticket
       params = "username=#{username}&password=#{password}&lt=#{ticket}"
-      status,response = connection.post('/login', params)#.last
-      (status.code == '200') ? username : nil
+      params << '&_eventId=submit&submit=LOGIN' if jasig
+      status,response =
+          connection.post("/#{no_slash_path}/login#{jasig}", params)
+
+      if jasig
+        (response =~ /Log In Successful/) ? username : nil
+      else
+        status.code == '200' ? username : nil
+      end
     end
 
     def acquire_ticket
-      login_page = connection.get('/login')
-      Hpricot(login_page.body).search('input[@id=lt]').first['value']
+      login_page = connection.get("/#{no_slash_path}/login")
+      ticket = Hpricot(login_page.body).search('input[@name=lt]').first
+      jasig_eats_babies(login_page.body)
+      ticket ? ticket['value'] : nil
+    end
+
+    def jasig_eats_babies(body)
+      @jasig = ';'
+      @jasig << Hpricot(body).search('form[@id=fm1]').
+                    first['action'].split(';').last
     end
 
     def connection
-      http = Net::HTTP.new(server_path, port)
-      http.use_ssl = true
+      http = Net::HTTP.new(hostname, port)
+      http.use_ssl = @https
       http
     end
 
     def authenticate_ticket(ticket)
-      connection.get("/serviceValidate?service=#{callback_url}&ticket=#{ticket}").body
+      connection.
+        get("/#{no_slash_path}/serviceValidate?service=#{callback_url}" +
+            "&ticket=#{ticket}").
+        body
     end
 
     def user_login(ticket)
-      user = Hpricot::XML(authenticate_ticket(ticket)).search('//cas:authenticationSuccess //cas:user').text
+      user = Hpricot::XML(authenticate_ticket(ticket)).
+                search('//cas:authenticationSuccess //cas:user').text
       user.strip != '' ? user : nil
     end
 
     def server_url
-      "https://#{server_path}:#{port}"
+      "#{protocol}://#{hostname}:#{port}/#{no_slash_path}"
+    end
+
+    def protocol
+      https ? 'http' : 'https'
+    end
+
+    def no_slash_path
+      path[0] == 47 ? path[1..path.size] : path
     end
 
   end
--- test/test_casual.rb
+++ test/test_casual.rb
@@ -1,18 +1,19 @@
 require 'helper'
 
 class TestCasual < Test::Unit::TestCase
-  
+
   def setup
-    @client = Casual::Client.new(:server_path => 'localhost', 
-                                 :port => 443, 
+    @client = Casual::Client.new(:hostname     => 'localhost',
+                                 :path         => '/cas',
+                                 :port         => '8080',
                                  :callback_url => 'http://localhost')
   end
-  
+
   def test_authorization_url
-    assert_equal @client.authorization_url, 
-      "https://localhost:443/login?service=http://localhost"
+    assert_equal @client.authorization_url,
+      "https://localhost:8080/cas/login?service=http://localhost"
   end
-  
+
   def test_user_login_success
     mock(@client).authenticate_ticket.with('GOLDEN_TICKET_LOL') {
       '<cas:serviceResponse xmlns:cas="http://www.yale.edu/tp/cas">'+
@@ -21,7 +22,7 @@ class TestCasual < Test::Unit::TestCase
     }
     assert_equal @client.user_login('GOLDEN_TICKET_LOL'), 'holman'
   end
-  
+
   def test_user_login_fail
     mock(@client).authenticate_ticket.with('GOLDEN_TICKET_FAIL') {
       '<cas:serviceResponse xmlns:cas="http://www.yale.edu/tp/cas">'+
@@ -30,9 +31,9 @@ class TestCasual < Test::Unit::TestCase
     }
     assert_equal @client.user_login('GOLDEN_TICKET_FAIL'), nil
   end
-  
+
   def test_authenticate_success
-    connection_mock = mock('').post.with_any_args { 
+    connection_mock = mock('').post.with_any_args {
       [Net::HTTPSuccess.new('','200',''), '<div class="messagebox confirmation">You have successfully logged in.</div>']
     }
     mock(@client).connection { connection_mock }
